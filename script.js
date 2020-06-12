3d75735303cc3e2150fc0282abda95fda5641207
Shanaun <sclayton1183@gmail.com>
2020-06-11 23:35:13 -0700

    conditional rendering

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/Greensocks.jpg',
-        altText: 'green socks'
+        altText: 'green socks',
+        // link: 'https://www.amazon.com/Rambutan-Leaves-Bamboo-Seamless-8-5-12-5/dp/B00KAHGDA4/ref=sr_1_6?dchild=1&keywords=green+socks&qid=1591942969&sr=8-6'
+        inStock: true,
+        onSale: true,
     }
 })
