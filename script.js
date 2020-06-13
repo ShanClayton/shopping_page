e48b7cdfdabdf8607b49c47b6f209c78b4162204
Shanaun <sclayton1183@gmail.com>
2020-06-12 23:50:50 -0700

    class and style binding

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ var app = new Vue({
         product: 'Socks',
         image: './assets/Greensocks.jpg',
         altText: 'green socks',
-        // link: 'https://www.amazon.com/Rambutan-Leaves-Bamboo-Seamless-8-5-12-5/dp/B00KAHGDA4/ref=sr_1_6?dchild=1&keywords=green+socks&qid=1591942969&sr=8-6'
-        inStock: true,
+        inStock: false,
         onSale: true,
         details: ["75% bamboo", "22% polyamide", "3% spandex", "Gender-neutral"],
         variants: [{
@@ -37,5 +36,13 @@ var app = new Vue({
     }
 })
 
+//NOTES AND CODE DUMP
 //not all browsers will support es6 version of function addToCart: function ()
 // sizes: ['S', 'M', 'L', 'XL', '2XL', '3XL']
+// link: 'https://www.amazon.com/Rambutan-Leaves-Bamboo-Seamless-8-5-12-5/dp/B00KAHGDA4/ref=sr_1_6?dchild=1&keywords=green+socks&qid=1591942969&sr=8-6'
+//< span v-if= "onSale" > Sale!</span > 
+//< p v -if= "inventory > 10" > In Stock</p >
+// <p v-else-if="inventory <=10 && inventory > 0">Almost sold out! Hurry!</p>
+//<p v-else="OutStock">Out of Stock</p>
+//<button v-on: click='removeFromCart'>Remove</button>
+// <a : href="link" target="_blank">See more products like this</a> 
