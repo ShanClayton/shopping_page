8c5e56a3ef2ff08fda86f07d67ae3a59b74aae23
Shanaun <sclayton1183@gmail.com>
2020-06-13 23:53:53 -0700

    forms

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-//created component from details in data
+//forms
+Vue.component("product-review", {
+  template: `
+  <form class="review-form" @submit.prevent="onSubmit">
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+      </p>
+      
+      <p>
+        <label for="review">Review:</label>      
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+      
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+          
+      <p>
+        <input type="submit" value="Submit">  
+      </p>    
+    
+    </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+    };
+  },
+});
+
+//product details
 Vue.component("product-details", {
   props: {
     details: {
@@ -12,7 +52,7 @@ Vue.component("product-details", {
     </ul>`,
 });
 
-//product info goes here
+//product info
 Vue.component("product", {
   props: {
     premium: {
@@ -46,8 +86,11 @@ Vue.component("product", {
                 <button v-on:click='addToCart' :disabled='!inStock' :class="{disabledButton: !inStock}">Add to
                     Cart</button>
                 <button v-on:click='removeFromCart'>Remove Item</button>
+
                 
             </div>
+            <!-- nesting component in product info template so it will show up underneath -->
+            <product-review></product-review>
         </div>`,
 
   //data goes here
